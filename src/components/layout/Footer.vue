87b3d727db218692429be521e306c93dd0f2396f
Gabriel Carlos <gabrielcarlos91@hotmail.com>
2024-02-18 15:46:29 -0300

    feat: created component footer

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <footer class="w-full bg-gray-50 py-6">
+        <p class="block text-gray-500 sm:text-center text-normal text-sm items-center">
+            <svg
+                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="mr-1 inline-block h-4 w-4">
+                <circle cx="12" cy="12" r="10"></circle>
+                <path d="M15 9.354a4 4 0 1 0 0 5.292"></path>
+            </svg>
+            2023 |&nbsp;<a target="_blank"
+            class="underline underline-offset-4 transition-all hover:text-gray-900 active:text-gray-600"
+            href="@">Designed</a>&nbsp;and&nbsp;<a
+            target="_blank" class="underline underline-offset-4 transition-all hover:text-gray-900 active:text-gray-600"
+            href="#">coded</a>&nbsp;with ❤️️ by Gabriel Carlos</p>
+    </footer>
+</template>
