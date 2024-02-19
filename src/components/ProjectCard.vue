ac88df177e123fde0aad2505717dcbd845e1e03e
Gabriel Carlos <gabrielcarlos91@hotmail.com>
2024-02-18 21:30:43 -0300

    feat: created component projects card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,6 +1,32 @@
 <script setup lang="ts">
-import { defineProps } from 'vue';
-const { project } = defineProps(["project"])
+import { useGithub } from '@/composable/useGithub';
+import { defineProps, onMounted } from 'vue';
+
+const { project } = defineProps(["project"]);
+const { getProjectLanguages } = useGithub();
+
+onMounted(async () => {
+    try {
+        const response = await getProjectLanguages(project.name);
+        console.log("RESPONSE", response);
+        project.tags = Object.keys(response);
+        console.log(project.private);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+const getTagValue = (tag: string) => {
+  const tagMap: any = {
+    'C#': 'c%23',
+    'C++': 'C++',
+    'Objective-C': 'OBJECTIVE--C',
+    'C': 'C'
+  };
+  
+  return tagMap[tag] || tag;
+};
+
 </script>
 
 <template>
@@ -10,13 +36,18 @@ const { project } = defineProps(["project"])
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {{ project.name }}</h5>
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> {{ project.description }}</p>
-        <a :href="project.html_url" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-        </a>
+        <div class="flex flex-wrap gap-2">
+            <img v-for="tag in project.tags" :key="tag" alt="language" :src="`https://img.shields.io/badge/${getTagValue(tag)}-0D1117?style=for-the-badge&logo=${getTagValue(tag)}`" />
+        </div>
     </div>
+    <div class="p-5 mt-auto">
+            <a :href="project.html_url" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Read more
+                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+        </div>
 </div>
 
 </template>
--- src/composable/useGithub.ts
+++ src/composable/useGithub.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
+const token = import.meta.env.VITE_GIT_TOKEN;
+const headers = {
+    Authorization: `Bearer ${token}`
+};
 
 export function useGithub() {
     
     async function getProjects(){
-        return axios.get('https://api.github.com/users/GabrielCSTR/repos')
+        return axios.get('https://api.github.com/users/GabrielCSTR/repos', { headers })
             .then(res => res.data)
             .catch(err => {
                 console.log(err);
@@ -12,7 +16,16 @@ export function useGithub() {
     }
 
     async function getProjectTags(project: string){
-        return axios.get(`https://api.github.com/repos/GabrielCSTR/${project}/tags`)
+        return axios.get(`https://api.github.com/repos/GabrielCSTR/${project}/tags`, { headers })
+            .then(res => res.data)
+            .catch(err => {
+                console.log(err);
+                return null;
+            });
+    }
+
+    async function getProjectLanguages(project: string){
+        return axios.get(`https://api.github.com/repos/GabrielCSTR/${project}/languages`, { headers })
             .then(res => res.data)
             .catch(err => {
                 console.log(err);
@@ -22,6 +35,7 @@ export function useGithub() {
 
     return {
         getProjects,
-        getProjectTags
+        getProjectTags,
+        getProjectLanguages
     }
 }
